0aeab879bb8e2b3934c009819ed8b766cc5d437b
nabitbol <noam1.abitbol@gmail.com>
2026-04-26 13:37:31 +0200

    fix(ping): fixed indentation issues

--- apps/ping/scrs/engine.c
+++ apps/ping/scrs/engine.c
@@ -20,8 +20,15 @@ void handle_sigint(int sig)
 static double ft_sqrt(double n)
 {
 	double x = n, y = 1, e = 0.000001;
-	if (n < 0) return 0;
-	while (x - y > e) { x = (x + y) / 2; y = n / x; }
+
+	if (n < 0)
+		return 0;
+
+	while (x - y > e)
+	{
+		x = (x + y) / 2;
+		y = n / x;
+	}
 	return x;
 }
 
@@ -36,17 +43,21 @@ static void print_stats(t_ping *p)
 
 	printf("\n--- %s ping statistics ---\n", p->dest_name);
 	printf("%d packets transmitted, %d packets received, %.0f%% packet loss, time %ldms\n",
-	       p->packets_sent, p->packets_received,
-	       (p->packets_sent > 0) ? ((p->packets_sent - p->packets_received) / (double)p->packets_sent) * 100 : 0,
-	       total_time);
-	
+		   p->packets_sent, p->packets_received,
+		   (p->packets_sent > 0) ? ((p->packets_sent - p->packets_received) /
+									(double)p->packets_sent) *
+									   100
+								 : 0,
+		   total_time);
+
 	if (p->packets_received > 0)
 	{
 		avg = p->sum_rtt / p->packets_received;
 		var = (p->sum_sq_rtt / p->packets_received) - (avg * avg);
 		stddev = ft_sqrt(var);
+
 		printf("round-trip min/avg/max/stddev = %.3f/%.3f/%.3f/%.3f ms\n",
-		       p->min_rtt, avg, p->max_rtt, stddev);
+			   p->min_rtt, avg, p->max_rtt, stddev);
 	}
 }
 
@@ -61,26 +72,39 @@ static void receive_one_ping(t_ping *p)
 	struct timeval *tv_s, tv_r;
 	double rtt;
 
-	if (recvfrom(p->sock, buf, sizeof(buf), 0, (struct sockaddr *)&f, &fl) < 0) return;
+	if (recvfrom(p->sock, buf, sizeof(buf), 0, (struct sockaddr *)&f, &fl) < 0)
+		return;
+
 	gettimeofday(&tv_r, NULL);
+
 	ip = (struct iphdr *)buf;
+
 	for (int i = 0; i < 8; i++)
 		raw_ic |= (uint64_t)((unsigned char)buf[(ip->ihl * 4) + i]) << (56 - (i * 8));
+
 	decode_datagram(raw_ic, &ic);
 	if (ic.type == ICMP_ECHOREPLY && ic.un.echo.id == p->id)
 	{
 		p->packets_received++;
 		tv_s = (struct timeval *)(buf + (ip->ihl * 4) + sizeof(struct icmphdr));
 		rtt = (tv_r.tv_sec - tv_s->tv_sec) * 1000.0 + (tv_r.tv_usec - tv_s->tv_usec) / 1000.0;
-		if (p->packets_received == 1 || rtt < p->min_rtt) p->min_rtt = rtt;
-		if (p->packets_received == 1 || rtt > p->max_rtt) p->max_rtt = rtt;
-		p->sum_rtt += rtt; p->sum_sq_rtt += rtt * rtt;
+
+		if (p->packets_received == 1 || rtt < p->min_rtt)
+			p->min_rtt = rtt;
+
+		if (p->packets_received == 1 || rtt > p->max_rtt)
+			p->max_rtt = rtt;
+
+		p->sum_rtt += rtt;
+		p->sum_sq_rtt += rtt * rtt;
+
 		printf("64 bytes from %s: icmp_seq=%d ttl=%d time=%.3f ms\n",
-		       p->dest_ip, ic.un.echo.sequence, ip->ttl, rtt);
+			   p->dest_ip, ic.un.echo.sequence, ip->ttl, rtt);
 	}
 	else if (p->verbose)
 	{
 		char pr_ip[INET_ADDRSTRLEN];
+
 		inet_ntop(AF_INET, &f.sin_addr, pr_ip, INET_ADDRSTRLEN);
 		printf("From %s: ", pr_ip);
 		print_icmp(ic.type);
@@ -106,10 +130,14 @@ void start_ping_loop(t_ping *p)
 		{
 			gettimeofday(&now, NULL);
 			el = (now.tv_sec - ls.tv_sec) * 1000000 + (now.tv_usec - ls.tv_usec);
-			if (el >= 1000000) break;
-			FD_ZERO(&r); FD_SET(p->sock, &r);
-			to.tv_sec = 0; to.tv_usec = 1000000 - el;
-			if (select(p->sock + 1, &r, NULL, NULL, &to) > 0) receive_one_ping(p);
+			if (el >= 1000000)
+				break;
+			FD_ZERO(&r);
+			FD_SET(p->sock, &r);
+			to.tv_sec = 0;
+			to.tv_usec = 1000000 - el;
+			if (select(p->sock + 1, &r, NULL, NULL, &to) > 0)
+				receive_one_ping(p);
 		}
 	}
 	print_stats(p);
